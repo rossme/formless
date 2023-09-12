133f3cfedb1e05543971cfb3ea17e474468dd9fd
Ross Buddie <66185926+rossme@users.noreply.github.com>
2023-09-12 15:09:10 +0100

    Add model specs and add additional validation (#8)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,32 @@ RSpec.describe User, type: :model do
       expect(user.email).to eq(user.email)
     end
 
-    it 'once persisted it can create new clients' do
+    it 'has many clients' do
+      expect(user.clients).to be_a(ActiveRecord::Associations::CollectionProxy)
+    end
+
+    it 'has many prompts' do
+      expect(user.prompts).to be_a(ActiveRecord::Associations::CollectionProxy)
+    end
+  end
+
+  context 'when user is persisted' do
+    let(:user) { build(:user) }
+
+    before do
       user.save!
+    end
+
+    it 'once persisted it can create new clients' do
       new_client = user.clients.create!(first_name: 'John', last_name: 'Doe')
       expect(user.reload.clients.last).to eq(new_client)
       expect(user.clients.count).to eq(1)
+    end
+
+    it 'once persisted it can create new prompts' do
+      new_prompt = user.prompts.create!(input: 'How many clients do I have?')
+      expect(user.reload.prompts.last).to eq(new_prompt)
+      expect(user.prompts.count).to eq(1)
     end
   end
 end
