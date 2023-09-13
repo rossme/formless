81b0fbec67fea53f174a11598b343e7ddf55210a
Ross <rbuddie@advicefront.com>
2023-09-13 23:25:39 +0100

    Added flash improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+
+  FLASH_LEVELS = {
+    notice: 'info',
+    success: 'success',
+    error: 'danger',
+    alert: 'warning'
+  }.freeze
+
+  def flash_class(level)
+    FLASH_LEVELS[level.to_sym]
+  end
 end
