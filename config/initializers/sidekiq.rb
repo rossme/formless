dbf138bbf54d2e5ced088b0329e8b988c47dd88d
Ross <rbuddie@advicefront.com>
2023-09-15 09:51:53 +0100

    Remove unused sidekiq.rb

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
-end
