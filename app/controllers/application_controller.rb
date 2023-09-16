682690ef65c1d5763a1473af368d1b4766e61492
Ross <rbuddie@advicefront.com>
2023-09-17 00:20:44 +0100

    Add fake user for devise - create fake session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :sign_in_fake_user # :authenticate_user!
+
+  FAKE_USER_ID = 1
+
+  private
+
+  def sign_in_fake_user
+    sign_in User.find_by(id: FAKE_USER_ID)
+  end
 end
