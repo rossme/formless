817db8c8f29e70ea38271db654c1807e733b675e
Ross Buddie <66185926+rossme@users.noreply.github.com>
2023-09-16 13:18:38 +0100

    Change timeoutable to 20 minutes (#21)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :timeoutable
 
   has_many :prompts, dependent: :destroy
   has_many :clients, dependent: :destroy
