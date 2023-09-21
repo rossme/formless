7415ec275072c18c9a391e9f9adf5c0e3fc14f9e
Ross <rbuddie@advicefront.com>
2023-09-21 10:15:44 +0100

    Move github release info to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,19 +9,25 @@ module ApplicationHelper
     alert: 'warning'
   }.freeze
 
-  def latest_github_release
+  def flash_class(level)
+    FLASH_LEVELS[level&.to_sym].presence
+  end
+
+  def github_release_details
+    raise StandardError, I18n.t('application.release_details.error') unless latest_release && latest_deploy
+
+    I18n.t('application.release_details.info', latest_release: latest_release, latest_deploy: latest_deploy)
+  end
+
+  def latest_release
     Octokit.latest_release('rossme/formless')&.tag_name
   end
 
-  def latest_deployed_release
+  def latest_deploy
     Octokit.deployments('rossme/formless').first[:updated_at]
   end
 
   def remote_ip_address
     request.remote_ip
-  end
-
-  def flash_class(level)
-    FLASH_LEVELS[level&.to_sym].presence
   end
 end
