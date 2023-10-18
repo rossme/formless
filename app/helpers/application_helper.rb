5993311e73689c02c7fc00270ecc1aafa3ea746d
Ross <rbuddie@advicefront.com>
2023-10-18 21:52:36 +0200

    Update Octokit credentials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,17 +14,22 @@ module ApplicationHelper
   end
 
   def github_release_details
-    raise StandardError, I18n.t('application.release_details.error') unless latest_release && latest_deploy
+    # Check if the personal access token has expired, Octokit then raises an exception
+    return I18n.t('application.release_details.error') unless latest_release && latest_deploy
 
     I18n.t('application.release_details.info', latest_release: latest_release, latest_deploy: latest_deploy)
   end
 
   def latest_release
     Octokit.latest_release('rossme/formless')&.tag_name
+  rescue Octokit::Unauthorized
+    nil # this error is handled in github_release_details
   end
 
   def latest_deploy
     Octokit.deployments('rossme/formless').first[:updated_at]
+  rescue Octokit::Unauthorized
+    nil # this error is handled in github_release_details
   end
 
   def remote_ip_address
