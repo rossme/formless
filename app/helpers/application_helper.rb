1bb38b3e153ca62201bd46020481f42b5abeacf9
Ross <rbuddie@advicefront.com>
2023-10-25 20:03:58 +0100

    Octokit caching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,25 +14,28 @@ module ApplicationHelper
   end
 
   def github_release_details
-    # Check if the personal access token has expired, Octokit then raises an exception
-    return I18n.t('application.release_details.error') unless latest_release && latest_deploy
+    return I18n.t('application.release_details.error') unless octokit_request
 
-    I18n.t('application.release_details.info', latest_release: latest_release, latest_deploy: latest_deploy)
-  end
-
-  def latest_release
-    Octokit.latest_release('rossme/formless')&.tag_name
-  rescue Octokit::Unauthorized
-    nil # this error is handled in github_release_details
-  end
-
-  def latest_deploy
-    Octokit.deployments('rossme/formless').first[:updated_at]
-  rescue Octokit::Unauthorized
-    nil # this error is handled in github_release_details
+    I18n.t('application.release_details.info',
+           latest_release: octokit_request[:latest_release],
+           latest_deploy: octokit_request[:latest_deploy]
+    )
   end
 
   def remote_ip_address
     request.remote_ip
+  end
+
+  # `rails dev:cache` toggles caching in development environment
+  def octokit_request
+    @octokit_request ||= Rails.cache.fetch('octokit_request', expires_in: 12.hours) do
+      Rails.logger.info(I18n.t('application.release_details.caching'))
+      {
+        latest_release: Octokit.latest_release('rossme/formless')&.tag_name,
+        latest_deploy: Octokit.deployments('rossme/formless').first[:updated_at]
+      }
+    end
+  rescue Octokit::Unauthorized, Octokit::TooManyRequests
+    nil # this error is handled in github_release_details
   end
 end
